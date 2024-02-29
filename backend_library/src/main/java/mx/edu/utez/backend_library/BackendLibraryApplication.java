41d8409d43e2c9de3a51c3d62c492bf24c2d724a
Ricardo Rodríguez González <20203tn145@utez.edu.mx>
2024-02-29 15:42:13 -0600

    First Commit
    Create backend project wit Spring Boot

---
+++ backend_library/src/main/java/mx/edu/utez/backend_library/BackendLibraryApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.backend_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLibraryApplication.class, args);
+	}
+
+}
