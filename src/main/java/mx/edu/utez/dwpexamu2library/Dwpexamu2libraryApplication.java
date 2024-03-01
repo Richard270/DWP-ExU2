c3c66bf8d4931a340b4ba2c9ee89b366a78a11f5
Ricardo Rodríguez González <20203tn145@utez.edu.mx>
2024-02-29 20:34:13 -0600

    First Commit
    Create backend project

---
+++ src/main/java/mx/edu/utez/dwpexamu2library/Dwpexamu2libraryApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.dwpexamu2library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dwpexamu2libraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dwpexamu2libraryApplication.class, args);
+	}
+
+}
